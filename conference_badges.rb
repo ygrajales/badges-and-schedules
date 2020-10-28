18dbce817de3d12dd5fe85ae4d932de642d6b236
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-10-28 14:59:30 -0500

    commit tree 127b36fbb9bc2f1fa4a06c295cbab88be02e1d0e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(participants)
+    participants.collect{|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(participants)
+    participants.collect{|name| "Hello, #{name}! You'll be assigned to room #{participants.index(name) + 1}!"}
+end
+
+def printer(participants)
+
+    badges = batch_badge_creator(participants)
+    rooms = assign_rooms(participants)
+
+    counter = 0
+
+    while counter < attendees.size
+        puts badges[counter]
+        puts rooms[counter]
+        counter += 1
+    end
+end
